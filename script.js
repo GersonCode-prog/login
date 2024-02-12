ceafd66fcf20ab5683de6e57f0670103f0bc09df
GersonCode-prog <batunsuriano7@gmail.com>
2024-02-12 11:09:49 -0600

    nuevo Login

--- script.js
+++ script.js
@@ -10,20 +10,4 @@ function login() {
     }
 }
 // Función para crear un copo de nieve
-function createSnowflake() {
-    const snowflake = document.createElement("div");
-    snowflake.classList.add("snowflake");
-    snowflake.innerHTML = "❆";
-    snowflake.style.left = Math.random() * window.innerWidth + "px";
-    snowflake.style.animationDuration = Math.random() * 3 + 2 + "s";
-    snowflake.style.opacity = Math.random();
-    snowflake.style.fontSize = Math.random() * 20 + 10 + "px";
-    document.body.appendChild(snowflake);
 
-    setTimeout(() => {
-        snowflake.remove();
-    }, 5000);
-}
-
-// Generar copos de nieve
-setInterval(createSnowflake, 100);
